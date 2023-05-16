68c827bc0ae83e7dcb76ccc470a76809d3163e0f
BrunoDelfim <brn.dfm@gmail.com>
2023-05-16 14:23:09 -0300

    login_action.php finish

---
+++ login_action.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'config.php';
+require 'models/Auth.php';
+
+/** @var object $base */
+/** @var object $pdo */
+
+// variável que irá armazenar os valores contidos nos inputs
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$password = filter_input(INPUT_POST, 'password');
+// se os inputs tiverem preenchidos
+if ($email && $password) {
+    // cria o objeto Auth
+    $auth = new Auth($pdo, $base);
+    // se o login for validado com sucesso
+    if ($auth->validateLogin($email, $password)) {
+        // encaminha o usuário para o feed
+        header("Location: ".$base);
+        // encerra a execução
+        exit;
+    }
+}
+// caso o login não for validado com sucesso retorna o usuário para a página de login
+header("Location: ".$base."/login.php");
+// encerra a execuçã
+exit;
