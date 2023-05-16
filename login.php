a185433417ade0856d8122b84b97edcd8bc0aa6d
BrunoDelfim <brn.dfm@gmail.com>
2023-05-16 11:48:56 -0300

    Added html content of login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Devsbook OO</title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
+    <link rel="stylesheet" href="assets/css/login.css" />
+</head>
+<body>
+<header>
+    <div class="container">
+        <a href=""><img src="assets/images/devsbook_logo.png"  alt="logo"/></a>
+    </div>
+</header>
+<section class="container main">
+    <form method="POST">
+        <label>
+            <input placeholder="Digite seu e-mail" class="input" type="email" name="email" />
+        </label>
+        <label>
+            <input placeholder="Digite sua senha" class="input" type="password" name="password" />
+        </label>
+        <label>
+            <input class="button" type="submit" value="Acessar o sistema" />
+        </label>
+        <a href="">Ainda não tem conta? Cadastre-se</a>
+    </form>
+</section>
+</body>
+</html>
