f4b2c6a29c5cc7fbbbe52766413f05b989d24d82
BrunoDelfim <brn.dfm@gmail.com>
2023-05-19 16:51:46 -0300

    finish day

--- partials/feed-editor.php
+++ partials/feed-editor.php
@@ -15,10 +15,10 @@
             <div class="feed-new-input-placeholder">O que você está pensando, <?=$firstName;?>?</div>
             <div class="feed-new-input" contenteditable="true"></div>
             <div class="feed-new-send">
-                <img src="<?=$base."/";?>assets/images/send.png"  alt=""/>
+                <img src="<?=$base."/";?>assets/images/send.png" class="send-arrow-feed"  alt=""/>
             </div>
             <form action="<?=$base."/";?>feed_editor_action.php" method="POST" class="feed-new-form">
-                <input type="hidden" name="body" />
+                <input type="hidden" name="body"/>
             </form>
         </div>
     </div>
@@ -27,16 +27,28 @@
     let feedInput = document.querySelector('.feed-new-input');
     let feedSubmit = document.querySelector('.feed-new-send');
     let feedForm = document.querySelector('.feed-new-form');
+    let arrowSendFeed = document.querySelector('.send-arrow-feed');
     let initial_value = feedInput.innerText.trim();
 
+    (function () {
+        let value = feedInput.innerText.trim();
+        if (initial_value === value) {
+            feedSubmit.style.cursor = 'default';
+            arrowSendFeed.style.opacity = '0.3';
+        }
+    })();
     feedInput.addEventListener('input', () => {
         let value = feedInput.innerText.trim();
         if (value !== initial_value) {
             feedSubmit.style.cursor = 'pointer';
+            arrowSendFeed.style.transition = 'opacity 0.3s ease-in-out';
+            arrowSendFeed.style.opacity = '1';
         } else {
             feedSubmit.style.cursor = 'default';
+            arrowSendFeed.style.opacity = '0.3';
         }
     });
+    // refatore o código abaixo para usar o método submit() do objeto form
     feedSubmit.addEventListener('click', () => {
         let value = feedInput.innerText.trim();
         if (value === initial_value) {
