9f9b3ca12d9d98dc25eafdcb4b3dd340d7f8ab5d
BrunoDelfim <brn.dfm@gmail.com>
2023-05-19 10:05:44 -0300

    aditor feed initialized and sponsorship inserted in the index

--- index.php
+++ index.php
@@ -16,7 +16,30 @@ require  'partials/menu.php';
 ?>
 
 <section class="feed mt-10">
-    ...
+    <div class="row">
+        <div class="column pr-5">
+            <?php require 'partials/feed-editor.php'; ?>
+        </div>
+        <div class="column side pl-5">
+            <div class="box banners">
+                <div class="box-header">
+                    <div class="box-header-text">Patrocínios</div>
+                    <div class="box-header-buttons">
+
+                    </div>
+                </div>
+                <div class="box-body">
+                    <a href=""><img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"  alt=""/></a>
+                    <a href=""><img src="https://cdn.pixabay.com/photo/2016/12/27/21/03/lone-tree-1934897__480.jpg"  alt=""/></a>
+                </div>
+            </div>
+            <div class="box">
+                <div class="box-body m-10">
+                    Criado com ❤️ por Bruno Delfim
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 <?php require 'partials/footer.php' ?>
